137ff88f5467900aa8c148759b13b4cc40aa2dd0
Mudavathv123 <123620615+Mudavathv123@users.noreply.github.com>
2025-10-24 13:20:33 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
-  const token = localStorage.getItem("token");
+  const token =  Cookies.get("jwtToken");
   const role = localStorage.getItem("role");
 
   if (!token || !allowedRoles.includes(role)) {
