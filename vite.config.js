c3e3caaafef0afc058049971fcea867ebb7aa251
Jahnavi29 <jahnaviboyina929@gmail.com>
2025-11-20 12:03:52 +0530

    modified .env

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // All requests starting with /api will be forwarded
       '/api': {
-        target: 'http://ec2-13-203-206-179.ap-south-1.compute.amazonaws.com:9090/',
+        target:'http://ec2-13-203-206-179.ap-south-1.compute.amazonaws.com:9090',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '/api') // optional, can keep the same
